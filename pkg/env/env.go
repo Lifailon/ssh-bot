25c09cff4e1abfa916ec8778eceb35bae34bd496
Lifailon <deadepicboss@gmail.com>
2025-07-04 09:55:18 +0300

    update env package

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -8,11 +8,16 @@ import (
 )
 
 type Env struct {
-	TELEGRAM_BOT_TOKEN string
-	TELEGRAM_USER_ID   int64
-	LOG_LEVEL          string
-	WIN_SHELL          string
-	LINUX_SHELL        string
+	TELEGRAM_BOT_TOKEN  string
+	TELEGRAM_USER_ID    int64
+	WIN_SHELL           string
+	LINUX_SHELL         string
+	SSH_USER            string
+	SSH_PORT            string
+	SSH_HOSTS           string
+	SSH_HOST_LIST       []string
+	SSH_CONNECT_TIMEOUT string
+	LOG_MODE            string
 }
 
 func (env *Env) GetEnv() {
@@ -43,21 +48,50 @@ func (env *Env) GetEnv() {
 			TELEGRAM_USER_ID_STR := strings.TrimSpace(strings.Split(envValue, "#")[0])
 			TELEGRAM_USER_ID_INT, _ := strconv.ParseInt(TELEGRAM_USER_ID_STR, 10, 64)
 			env.TELEGRAM_USER_ID = TELEGRAM_USER_ID_INT
-		case envKey == "LOG_LEVEL":
-			env.LOG_LEVEL = strings.TrimSpace(strings.Split(envValue, "#")[0])
 		case envKey == "WIN_SHELL":
 			env.WIN_SHELL = strings.TrimSpace(strings.Split(envValue, "#")[0])
 		case envKey == "LINUX_SHELL":
 			env.LINUX_SHELL = strings.TrimSpace(strings.Split(envValue, "#")[0])
+		case envKey == "SSH_USER":
+			env.SSH_USER = strings.TrimSpace(strings.Split(envValue, "#")[0])
+		case envKey == "SSH_PORT":
+			env.SSH_PORT = strings.TrimSpace(strings.Split(envValue, "#")[0])
+		case envKey == "SSH_HOST_LIST":
+			env.SSH_HOSTS = strings.TrimSpace(strings.Split(envValue, "#")[0])
+		case envKey == "SSH_CONNECT_TIMEOUT":
+			env.SSH_CONNECT_TIMEOUT = strings.TrimSpace(strings.Split(envValue, "#")[0])
+		case envKey == "LOG_MODE":
+			env.LOG_MODE = strings.TrimSpace(strings.Split(envValue, "#")[0])
 		}
 	}
 
-	if env.LOG_LEVEL == "DEBUG" {
-		log.Println()
-		log.Println("TELEGRAM_BOT_TOKEN: " + env.TELEGRAM_BOT_TOKEN)
-		log.Printf("TELEGRAM_USER_ID: %d \n", env.TELEGRAM_USER_ID)
-		log.Println("WIN_SHELL: " + env.WIN_SHELL)
-		log.Println("LINUX_SHELL: " + env.LINUX_SHELL)
-		log.Println()
+	env.GetHost()
+
+	if env.LOG_MODE == "DEBUG" {
+		env.PrintEnv()
 	}
+}
+
+func (env *Env) GetHost() {
+	env.SSH_HOST_LIST = []string{}
+	hosts := strings.Split(env.SSH_HOSTS, ",")
+	for _, host := range hosts {
+		env.SSH_HOST_LIST = append(env.SSH_HOST_LIST, strings.TrimSpace(host))
+	}
+}
+
+func (env *Env) PrintEnv() {
+	log.Println()
+	log.Println("[ENV] TELEGRAM_BOT_TOKEN: " + env.TELEGRAM_BOT_TOKEN)
+	log.Printf("[ENV] TELEGRAM_USER_ID: %d \n", env.TELEGRAM_USER_ID)
+	log.Println("[ENV] WIN_SHELL: " + env.WIN_SHELL)
+	log.Println("[ENV] LINUX_SHELL: " + env.LINUX_SHELL)
+	log.Println("[ENV] SSH_USER: " + env.SSH_USER)
+	log.Println("[ENV] SSH_PORT: " + env.SSH_PORT)
+	log.Println("[ENV] SSH_HOST_LIST:")
+	for _, host := range env.SSH_HOST_LIST {
+		log.Println("[ENV] - " + host)
+	}
+	log.Println("[ENV] SSH_CONNECT_TIMEOUT: " + env.SSH_CONNECT_TIMEOUT)
+	log.Println()
 }
